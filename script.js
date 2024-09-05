dacbbfc5e442379a30cdca6572a2da9ee6f95b78
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2024-09-05 01:36:02 -0300

    portfolio

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
 const yearSpan = document.getElementById('copyright');
 const copyright = new Date().getFullYear();
 yearSpan.textContent = copyright;
+
+
+/*  typing text code */
+
+var sliderCounter = 0;
+var sliderContent = [
+  "Developer",
+  "Designer",
+  "youtube",
+  "Code"
+];
+
+var sliderValue = document.querySelector("#SliderValue");
+
+function slide() {
+
+  if (sliderCounter >= sliderContent.length) {
+    sliderCounter = 0;
+  }
+
+  sliderValue.innerHTML = ""
+  sliderValue.classList.remove("holder-animation");
+  void sliderValue.offsetWidth;
+  sliderValue.classList.add("holder-animation");
+
+  for (i = 0; i < sliderContent[sliderCounter].length; i++) {
+    let letterDiv = document.createElement("div");
+    letterDiv.innerHTML = sliderContent[sliderCounter][i];
+
+    if (letterDiv.innerHTML == "") {
+      letterDiv.innerHTML = "&nbsp;";
+    }
+
+    letterDiv.classList.add("start");
+    letterDiv.classList.add("animation");
+    letterDiv.style.animationDelay = i / 10 + "s";
+    sliderValue.appendChild(letterDiv)
+  }
+
+  sliderCounter++;
+
+}
+
+slide()
+setInterval(slide, 2000);
